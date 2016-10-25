e06fcfe9379910f923fd2874526e974f381c6b35
Arthur Barrett <abarrett@fas.harvard.edu>
2016-10-25 12:08:25 -0400

    Updated nginx configuration.

--- src/saml-sp/app/protected.php
+++ src/saml-sp/app/protected.php
@@ -1,6 +1,6 @@
 <?php
 require_once('../simplesamlphp/lib/_autoload.php');
-
+#print "<pre>";print_r($_SERVER);print "</pre>";exit(1);
 $as = new SimpleSAML_Auth_Simple('default-sp');
 $as->requireAuth();
 $attributes = $as->getAttributes();
